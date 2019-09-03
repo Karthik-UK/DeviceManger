f42cc3a64b97c1f4ce0c240de504756ecc844a0c
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-09-03 17:37:21 +0530

    first commit

---
+++ DeviceManager/DeviceManager/View/BaseVC.swift
@@ -0,0 +1,19 @@
+//
+//  BaseVC.swift
+//  DeviceManager
+//
+//  Created by Karthik UK on 03/09/19.
+//  Copyright © 2019 Karthik UK. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ DeviceManager/DeviceManager/View/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  DeviceManager
+//
+//  Created by Karthik UK on 03/09/19.
+//  Copyright © 2019 Karthik UK. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
