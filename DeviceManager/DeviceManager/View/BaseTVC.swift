4f946d743e4db027e58074cc6c382407148c7597
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-09-03 20:02:52 +0530

    fourth  commit

---
+++ DeviceManager/DeviceManager/View/BaseTVC.swift
@@ -0,0 +1,24 @@
+//
+//  BaseTVC.swift
+//  DeviceManager
+//
+//  Created by Karthik UK on 03/09/19.
+//  Copyright © 2019 Karthik UK. All rights reserved.
+//
+
+import UIKit
+
+class BaseTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DeviceManager/DeviceManager/View/LoginVC.swift
+++ DeviceManager/DeviceManager/View/LoginVC.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
-class LoginVC: BaseVC {
-
+let lll = ["email" , "Pass"]
+class LoginVC:BaseVC,UITableViewDelegate,UITableViewDataSource{
+@IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: LoginCell.self), for: indexPath) as? LoginCell {
+            cell.cellLabel.text = lll[indexPath.row]
+            //            cell.imageOutlet.image = UIImage(named: newsModel.news[index.row].imagenews)
+            //            cell.labelOutlet.text = newsModel.news[index.row].categorynews
+            //            cell.labelExplain.text = newsModel.news[index.row].descriptionnews
+            //            cell.selectionStyle = .none
+            return cell
+        }
+        return LoginCell()
+        
+}
 }
