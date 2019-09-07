946c621de18af17f0e2054d8a9ca12d6d5303819
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-09-07 15:44:14 +0530

    Login  modifications 5

--- DeviceManager/DeviceManager/View-Model/LoginVM.swift
+++ DeviceManager/DeviceManager/View-Model/LoginVM.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class LoginVM {
     var loginInfo: [LoginFields] = []
+    var email = ""
     func login() {
         let details: [LoginFields] = [LoginFields(title: "Email"), LoginFields(title: "Password" )]
         loginInfo = details
--- DeviceManager/DeviceManager/View/Login /LoginVC.swift
+++ DeviceManager/DeviceManager/View/Login /LoginVC.swift
@@ -12,17 +12,19 @@ import GoogleSignIn
 
 
 class LoginVC: BaseVC, UITableViewDelegate, UITableViewDataSource , UITextFieldDelegate {
-    let labelField = LoginVM()
+    let loginvm = LoginVM()
+    let emailverification = EmailVerification()
+    //let firebasemanager = FireBaseManager()
 //    @IBAction func forgotPassword(_ sender: Any) {
 //        if let ForgotViewController = UIStoryboard(name: "ForgotPasswordVC", bundle:
     //nil).instantiateViewController(withIdentifier: String(describing: "ForgotPasswordVC")) as? ForgotPasswordVC { return }
 //        self.navigationController?.pushViewController(forgotViewController, animated: true)
 //    }
     var currentIndex : IndexPath?
-    var email: String = ""
-    var password: String = ""
+
     @IBAction func manualLogin(_ sender: Any) {
-       // guard let email = contactPointTextField.text, let password = passwordTextField.text else //{ return }
+       emailverification.verifyemail(mail: loginvm.email)
+      
         
     }
     @IBOutlet weak var tableView: UITableView!
@@ -33,35 +35,36 @@ class LoginVC: BaseVC, UITableViewDelegate, UITableViewDataSource , UITextFieldD
         NotificationCenter.default.addObserver(self, selector: #selector(navigateDashboard), name: NSNotification.Name(rawValue: "Success"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(wrongncredential), name: NSNotification.Name(rawValue: "Failure"), object: nil)
       
-        Analytics.logEvent("GoogleLogin", parameters: ["MODULE": "LoginVC",
-                                                 "STATUS": "TRUE"])
+        
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelField.login()
-        
-        
+        loginvm.login()
     }
     
     @objc func navigateDashboard() {
+        Analytics.logEvent("GoogleLogin", parameters: ["MODULE": "LoginVC",
+                                                       "STATUS": "TRUE"])
         guard let DashBoard = UIStoryboard(name: "Dashboard", bundle: nil).instantiateViewController(withIdentifier: "DashBoardVC") as? DashBoardVC else { return }
         self.present(DashBoard, animated: true, completion: nil)
     }
     @objc func wrongncredential() {
-        showAlert(message: "Wrong Credentials", title: "Email dosent Exist ", type: .alert ,action :[AlertAction(title:"OK",style: .default ,handler: nil)])
+        Analytics.logEvent("GoogleLogin", parameters: ["MODULE": "LoginVC",
+                                                       "STATUS": "False"])
+        showAlert(message: "Wrong Credentials", title: "Email doesn't Exist ", type: .alert ,action :[AlertAction(title:"OK",style: .default ,handler: nil)])
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return labelField.loginInfo.count
+        return loginvm.loginInfo.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: LoginCell.self), for: indexPath) as? LoginCell {
             currentIndex = indexPath
             cell.selectionStyle = .none
             cell.cellTextField.tag = indexPath.row
-            cell.cellLabel.text = labelField.loginInfo[indexPath.row].title
+            cell.cellLabel.text = loginvm.loginInfo[indexPath.row].title
             cell.cellTextField.delegate = self
 
                 if indexPath.row == 0 {
@@ -80,12 +83,17 @@ class LoginVC: BaseVC, UITableViewDelegate, UITableViewDataSource , UITextFieldD
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //use this in button
         if currentIndex == IndexPath(row: 0, section: 0) {
             if let index = currentIndex {
                 if let cell = tableView.cellForRow(at: index) as? LoginCell
                 { let text = cell.cellTextField.text
                 if text!.isValid(.email) {
                     cell.cellLabel.textColor = .blue
+                    guard let mail = cell.cellTextField.text else {return}
+                    loginvm.email = mail
+
                 } else {
                     cell.cellLabel.textColor = .red
                    
